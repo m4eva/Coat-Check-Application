f0de0cba306c2bdb6d1ca1a6afd8469ab76fb904
m4eva1 <nguedia.jm@gmail.com>
2026-07-22 18:30:47 -0400

    feat: Add routes for Player pages

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 def index():
   return render_template('base_player.html')
 
+@app.route('/home')
+def home():
+  return render_template('base_player.html')
+
+@app.route('/number/<int:num>')
+def number(num):
+  return render_template('base_player.html', num=num)
+
 
 if __name__ == "__main__":
   app.run(debug=True)
