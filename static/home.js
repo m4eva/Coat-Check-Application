1c9c45ad049e99b202b61d148c3682a421d019c2
m4eva1 <nguedia.jm@gmail.com>
2026-08-26 13:36:21 -0400

    refactor: Seperate the scripts

---
+++ static/home.js
@@ -0,0 +1,23 @@
+//VARIABLES
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  //DOCUMENT ELEMENTS
+  const coatNumbers = document.getElementById("coat-numbers");
+
+  //EVENT LISTENERS
+  coatNumbers.addEventListener("click", (e) => {changePage(e)});
+});
+
+//FUNCTIONS
+function changePage(e) {
+  let numDiv = e.target.closest(".number");
+
+  if (numDiv.classList.contains("assigned")) {
+  }
+  else if (numDiv.classList.contains("unassigned")) {
+    e.preventDefault();
+    // TODO: Instead of alert, make it a pop-up that fades away (or with X)
+    alert("Number not assigned yet.");
+  }
+}
--- static/number.js
+++ static/number.js
@@ -3,26 +3,13 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   //DOCUMENT ELEMENTS
-  const coatNumbers = document.getElementById("coat-numbers");
   const unassign = document.getElementById("unassign");
 
   //EVENT LISTENERS
-  coatNumbers.addEventListener("click", (e) => {changePage(e)});
   unassign.addEventListener("click", (e) => {unassignNumber(e)});
 });
 
 //FUNCTIONS
-function changePage(e) {
-  let numDiv = e.target.closest(".number");
-
-  if (numDiv.classList.contains("assigned")) {
-  }
-  else if (numDiv.classList.contains("unassigned")) {
-    e.preventDefault();
-    // TODO: Instead of alert, make it a pop-up that fades away (or with X)
-    alert("Number not assigned yet.");
-  }
-}
 function unassignNumber(e) {
   const confirm = window.confirm("Are you sure you want to unassign this number?");
   if (confirm) {
